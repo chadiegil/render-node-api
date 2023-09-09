b1958e714a8105fd5be57ebc0c8e8d8a7e6bb825
Chadie Gil Augis <caugis@nerubia.com>
2023-09-09 14:40:46 +0800

    added new data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ const express = require("express")
 const app = express()
 const port = 3000 // You can change this port number if needed
 
+const data = [
+  {
+    name: "Chadie Gil",
+    age: "28",
+    address: "USA",
+  },
+  {
+    name: "Regene",
+    age: "21",
+    address: "Canada",
+  },
+]
+
 // Define a route that responds with "Hello, World!"
 app.get("/", (req, res) => {
-  res.send("Hello, World!")
+  res.json(data)
 })
 
 // Start the server
